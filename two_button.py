845eb6cf45a4bfb481026d72a3255bd0a9d27b35
Ximeng Zhang <ximengzhang@dhcp-rhodes-2103.redrover.cornell.edu>
2020-02-28 18:18:56 -0500

    moved function

--- two_button.py
+++ two_button.py
@@ -3,6 +3,60 @@ from pygame.locals import *   # for event MOUSE variables
 import os
 import time
 import RPi.GPIO as GPIO
+def displaycollide():
+    size = width, height = 320, 240 
+    speed = [2,2]
+    speed1 = [-1,-1] 
+    inits = [0,10]
+    inits1 = [200,10]
+    black = 0, 0, 0
+    screen = pygame.display.set_mode(size)
+    ball = pygame.image.load("magic_ball.png")
+    ballrect = ball.get_rect()
+
+    ballrect = ballrect.move(inits)
+    ball1 = pygame.image.load("soccer_ball.png")
+    ballrect1 = ball1.get_rect()
+    ballrect1 = ballrect1.move(inits1)
+
+    start = time.time()
+    flag1 = True  # global flag
+    ballrect1.move(inits1)
+
+    while (flag1):    
+        ballrect = ballrect.move(speed)    
+        if ballrect.left < 0 or ballrect.right > width:       
+            speed[0] = -speed[0]    
+        if ballrect.top < 0 or ballrect.bottom > height:     
+            speed[1] = -speed[1]
+        ballrect1 = ballrect1.move(speed1)    
+        if ballrect1.left < 0 or ballrect1.right > width:       
+            speed1[0] = -speed1[0]    
+        if ballrect1.top < 0 or ballrect1.bottom > height:     
+            speed1[1] = -speed1[1]
+        if (ballrect.colliderect(ballrect1) == True):
+            tmp,tmp1 = speed1[0],speed1[1]
+            speed1[0] = speed[0]
+            speed1[1] = speed[1]
+            speed[0] = tmp
+            speed[1] = tmp1
+        time.sleep(0.01)
+        screen.fill(black)               # Erase the Work space   
+        screen.blit(ball, ballrect)   # Combine Ball surface with workspace 
+        screen.blit(ball1, ballrect1)
+
+        if(not time.time() - start < 10):
+            flag1 = False
+        if(not GPIO.input(27)):
+            flag1 = False
+        pygame.display.flip()
+        pos = pygame.mouse.get_pos() 
+        x,y = pos
+        if y > 120:                
+            if x < 160:    
+                print ('quit button pressed')
+                flag1 = False
+                
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(27,GPIO.IN,pull_up_down = GPIO.PUD_UP)   
 os.putenv('SDL_VIDEODRIVER', 'fbcon')   # Display on piTFT
@@ -57,56 +111,4 @@ while flag:
     if(not GPIO.input(27)):
 		flag = False
 
-def displaycollide():
-    size = width, height = 320, 240 
-    speed = [2,2]
-    speed1 = [-1,-1] 
-    inits = [0,10]
-    inits1 = [200,10]
-    black = 0, 0, 0
-    screen = pygame.display.set_mode(size)
-    ball = pygame.image.load("magic_ball.png")
-    ballrect = ball.get_rect()
 
-    ballrect = ballrect.move(inits)
-    ball1 = pygame.image.load("soccer_ball.png")
-    ballrect1 = ball1.get_rect()
-    ballrect1 = ballrect1.move(inits1)
-
-    start = time.time()
-    flag1 = True  # global flag
-    ballrect1.move(inits1)
-
-    while (flag1):    
-        ballrect = ballrect.move(speed)    
-        if ballrect.left < 0 or ballrect.right > width:       
-            speed[0] = -speed[0]    
-        if ballrect.top < 0 or ballrect.bottom > height:     
-            speed[1] = -speed[1]
-        ballrect1 = ballrect1.move(speed1)    
-        if ballrect1.left < 0 or ballrect1.right > width:       
-            speed1[0] = -speed1[0]    
-        if ballrect1.top < 0 or ballrect1.bottom > height:     
-            speed1[1] = -speed1[1]
-        if (ballrect.colliderect(ballrect1) == True):
-            tmp,tmp1 = speed1[0],speed1[1]
-            speed1[0] = speed[0]
-            speed1[1] = speed[1]
-            speed[0] = tmp
-            speed[1] = tmp1
-        time.sleep(0.01)
-        screen.fill(black)               # Erase the Work space   
-        screen.blit(ball, ballrect)   # Combine Ball surface with workspace 
-        screen.blit(ball1, ballrect1)
-
-        if(not time.time() - start < 10):
-            flag1 = False
-        if(not GPIO.input(27)):
-            flag1 = False
-        pygame.display.flip()
-        pos = pygame.mouse.get_pos() 
-        x,y = pos
-        if y > 120:                
-            if x < 160:    
-                print ('quit button pressed')
-                flag1 = False
