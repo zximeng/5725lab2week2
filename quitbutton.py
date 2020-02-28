bc584166fd31494cbce3b1e76d7eb71d829e1f85
Ximeng Zhang <ximengzhang@dhcp-rhodes-2103.redrover.cornell.edu>
2020-02-28 15:16:30 -0500

    added button

---
+++ quitbutton.py
@@ -0,0 +1,32 @@
+import pygame
+from pygame.locals import *   # for event MOUSE variables
+import os
+os.putenv('SDL_VIDEODRIVER', 'fbcon')   # Display on piTFTos.putenv('SDL_FBDEV', '/dev/fb1')     
+os.putenv('SDL_MOUSEDRV', 'TSLIB')     # Track mouse clicks on piTFT
+os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
+pygame.init()
+pygame.mouse.set_visible(False)
+WHITE = 255, 255, 255
+BLACK = 0,0,0
+screen = pygame.display.set_mode((320, 240))
+my_font= pygame.font.Font(None, 50)
+my_buttons= { 'button1':(80,180), 'button2':(240,180)}
+screen.fill(BLACK)               # Erase the Work space     
+for my_text, text_pos in my_buttons.items():    
+    text_surface = my_font.render(my_text, True, WHITE)    
+    rect = text_surface.get_rect(center=text_pos)
+    screen.blit(text_surface, rect)
+pygame.display.flip()
+
+# while True:       
+#     for event in pygame.event.get():        
+#         if(event.type is MOUSEBUTTONDOWN):            
+#             pos = pygame.mouse.get_pos()           
+#         elif(event.type is MOUSEBUTTONUP):            
+#             pos = pygame.mouse.get_pos() 
+#             x,y = pos
+#             if y > 120:                
+#                 if x < 160:    
+#                     print ('button1 pressed')              
+#                 else:                    
+#                     print ('button2 pressed')
