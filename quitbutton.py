bbb15726a895faa219cb3525af44c24c4be5776e
Ximeng Zhang <ximengzhang@dhcp-rhodes-2103.redrover.cornell.edu>
2020-02-28 16:14:10 -0500

    added button quit and time quit

--- quitbutton.py
+++ quitbutton.py
@@ -1,7 +1,10 @@
 import pygame
 from pygame.locals import *   # for event MOUSE variables
 import os
-os.putenv('SDL_VIDEODRIVER', 'fbcon')   # Display on piTFTos.putenv('SDL_FBDEV', '/dev/fb1')     
+import time
+import RPi.GPIO as GPIO
+os.putenv('SDL_VIDEODRIVER', 'fbcon')   # Display on piTFT
+os.putenv('SDL_FBDEV', '/dev/fb1')     
 os.putenv('SDL_MOUSEDRV', 'TSLIB')     # Track mouse clicks on piTFT
 os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
 pygame.init()
@@ -17,16 +20,20 @@ for my_text, text_pos in my_buttons.items():
     rect = text_surface.get_rect(center=text_pos)
     screen.blit(text_surface, rect)
 pygame.display.flip()
-
-# while True:       
-#     for event in pygame.event.get():        
-#         if(event.type is MOUSEBUTTONDOWN):            
-#             pos = pygame.mouse.get_pos()           
-#         elif(event.type is MOUSEBUTTONUP):            
-#             pos = pygame.mouse.get_pos() 
-#             x,y = pos
-#             if y > 120:                
-#                 if x < 160:    
-#                     print ('button1 pressed')              
-#                 else:                    
-#                     print ('button2 pressed')
+start = time.time()
+while True:       
+    for event in pygame.event.get():        
+        if(event.type is MOUSEBUTTONDOWN):            
+            pos = pygame.mouse.get_pos()           
+        elif(event.type is MOUSEBUTTONUP):            
+            pos = pygame.mouse.get_pos() 
+            x,y = pos
+            if y > 120:                
+                if x < 160:    
+                    print ('button1 pressed')              
+                else:                    
+                    print ('button2 pressed')
+    if(not time.time() - start < 3):
+		flag = False
+    if(not GPIO.input(27)):
+		flag = False
